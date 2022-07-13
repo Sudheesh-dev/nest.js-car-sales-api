407f69603bff708d7fc26513d2cb6001a4c38651
Sudheesh <sudheeshajayakumar493@gmail.com>
2022-07-13 18:23:45 +0530

    added typeorm and create user route

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,11 +1,11 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn, Unique } from "typeorm";
 
 @Entity()
 export class User{
     @PrimaryGeneratedColumn()
     id: number;
     @Column()
-    name:string;
+    email:string;
     @Column()
     password:string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { createUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+    constructor(
+        private userService: UsersService
+    ){}
+    @Post("/signup")
+    createUser(@Body() user: createUserDto){
+        console.log(user);
+        this.userService.create(user.email, user.password)
+    }
+}
